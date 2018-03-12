00161bfe8c4d6dfaae8721460f72014e8c57b0f5
ezio84 <brabus84@gmail.com>
2018-03-12 13:01:21 +0200

    Statusbar notification ticker [2/2]
    Change-Id: I8b4f6e8343c0d1b4260c5153d41a0d47bb38fe6c

--- src/mx/elixir/magicalworld/fragments/NotificationSettings.java
+++ src/mx/elixir/magicalworld/fragments/NotificationSettings.java
@@ -4,8 +4,10 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
@@ -17,6 +19,8 @@ import com.android.settings.SettingsPreferenceFragment;
 public class NotificationSettings extends SettingsPreferenceFragment
                         implements OnPreferenceChangeListener {
 
+    private ListPreference mTickerMode;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -24,9 +28,27 @@ public class NotificationSettings extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.elixir_settings_notifications);
 
         PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mTickerMode = (ListPreference) findPreference("ticker_mode");
+        mTickerMode.setOnPreferenceChangeListener(this);
+        int tickerMode = Settings.System.getIntForUser(getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_TICKER,
+                1, UserHandle.USER_CURRENT);
+        mTickerMode.setValue(String.valueOf(tickerMode));
+        mTickerMode.setSummary(mTickerMode.getEntry());
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference.equals(mTickerMode)) {
+            int tickerMode = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.STATUS_BAR_SHOW_TICKER, tickerMode, UserHandle.USER_CURRENT);
+            int index = mTickerMode.findIndexOfValue((String) newValue);
+            mTickerMode.setSummary(
+                    mTickerMode.getEntries()[index]);
+            return true;
+        }
         return false;
     }
 
