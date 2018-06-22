crdroidandroid/android_packages_apps_crDroidSettings

Custom inactivity threshold for network traffic monitor [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/crdroid/settings/fragments/statusbar/NetworkTrafficSettings.java
@@ -29,6 +29,7 @@ import lineageos.providers.LineageSettings;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.crdroid.settings.preferences.CustomSeekBarPreference;
 import com.crdroid.settings.R;
 
 public class NetworkTrafficSettings extends SettingsPreferenceFragment
@@ -37,7 +38,7 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
     private static final String TAG = "NetworkTrafficSettings";
 
     private ListPreference mNetTrafficMode;
-    private LineageSecureSettingSwitchPreference mNetTrafficAutohide;
+    private CustomSeekBarPreference mNetTrafficAutohide;
     private ListPreference mNetTrafficUnits;
     private LineageSecureSettingSwitchPreference mNetTrafficShowUnits;
 
@@ -54,8 +55,12 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
         mNetTrafficMode.setValue(String.valueOf(mode));
         mNetTrafficMode.setOnPreferenceChangeListener(this);
 
-        mNetTrafficAutohide = (LineageSecureSettingSwitchPreference)
+        mNetTrafficAutohide = (CustomSeekBarPreference)
                 findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
+        int mAutohideThreshold = LineageSettings.Secure.getIntForUser(resolver,
+                LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE, 0, UserHandle.USER_CURRENT);
+        mNetTrafficAutohide.setValue(mAutohideThreshold);
+        mNetTrafficAutohide.setOnPreferenceChangeListener(this);
 
         mNetTrafficUnits = (ListPreference)
                 findPreference(LineageSettings.Secure.NETWORK_TRAFFIC_UNITS);
@@ -74,6 +79,11 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
         if (preference == mNetTrafficMode) {
             int mode = Integer.valueOf((String) newValue);
             updateEnabledStates(mode);
+            return true;
+        } else if (preference == mNetTrafficAutohide) {
+            int value = (Integer) newValue;
+            LineageSettings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE, value, UserHandle.USER_CURRENT);
             return true;
         }
         return false;

jmjxperia/android_packages_apps_MagicalWorld

src/mx/mdroid/magicalworld/fragments/NetworkTrafficSettings.java
@@ -20,10 +20,12 @@ import com.android.internal.logging.nano.MetricsProto;
 
 import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.support.v7.preference.DropDownPreference;
 import android.support.v7.preference.Preference;
 
+import com.android.settings.mdroid.CustomSeekBarPreference;
 import com.android.settings.mdroid.SecureSettingSwitchPreference;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -35,7 +37,7 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
     private static final String TAG = "NetworkTrafficSettings";
 
     private DropDownPreference mNetTrafficMode;
-    private SecureSettingSwitchPreference mNetTrafficAutohide;
+    private CustomSeekBarPreference mNetTrafficAutohide;
     private DropDownPreference mNetTrafficUnits;
     private SecureSettingSwitchPreference mNetTrafficShowUnits;
 
@@ -52,8 +54,11 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
                 Settings.Secure.NETWORK_TRAFFIC_MODE, 0);
         mNetTrafficMode.setValue(String.valueOf(mode));
 
-        mNetTrafficAutohide = (SecureSettingSwitchPreference)
+        mNetTrafficAutohide = (CustomSeekBarPreference)
                 findPreference(Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE);
+        int mAutohideThreshold = Settings.Secure.getIntForUser(resolver,
+                Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE, 0, UserHandle.USER_CURRENT);
+        mNetTrafficAutohide.setValue(mAutohideThreshold);
         mNetTrafficAutohide.setOnPreferenceChangeListener(this);
 
         mNetTrafficUnits = (DropDownPreference)
@@ -81,6 +86,10 @@ public class NetworkTrafficSettings extends SettingsPreferenceFragment
             int units = Integer.valueOf((String) newValue);
             Settings.Secure.putInt(getActivity().getContentResolver(),
                     Settings.Secure.NETWORK_TRAFFIC_UNITS, units);
+        } else if (preference == mNetTrafficAutohide) {
+            int value = (Integer) newValue;
+            Settings.Secure.putIntForUser(getActivity().getContentResolver(),
+                    Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE, value, UserHandle.USER_CURRENT);
         }
         return true;
     }